1a4abd40ce2099e076af7f2cde15357c77640e35
Avujcic1 <44148907+Avujcic1@users.noreply.github.com>
2018-10-30 23:12:55 +0100

    Zapocete klase

---
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -0,0 +1,52 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Imenik {
+
+    HashMap<String, TelefonskiBroj> mapa;
+
+    public Imenik() {
+        mapa = new HashMap<>();
+    }
+
+    public void dodaj(String ime, TelefonskiBroj broj) {
+        mapa.put(ime, broj);
+    }
+
+    public String dajBroj(String ime) {
+        return mapa.get(ime).ispisi();
+    }
+
+    public String dajIme(TelefonskiBroj broj) {
+        return mapa.get(broj).ispisi();
+    }
+
+    public String naSlovo(char s) {
+        String temp = " ";
+        int brojac = 1;
+        for(Map.Entry<String, TelefonskiBroj> ulaz : mapa.entrySet()) {
+            if(ulaz.getKey().charAt(0) == s) {
+                temp += brojac + ". " + ulaz.getKey() + " - " + ulaz.getValue().ispisi() + "\n";
+                brojac++;
+            }
+        }
+        return temp;
+    }
+
+    public Set<String> izGrada(FiksniBroj g) {
+        Set<String> temp = new HashSet<>();
+        for(Map.Entry<String, TelefonskiBroj> ulaz : mapa.entrySet()) {
+
+        }
+    }
+
+    public Set<TelefonskiBroj> izGradaBrojevi(FiksniBroj g) {
+
+    }
+
+
+}
